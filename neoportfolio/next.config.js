651660fdeadbc91f135583e2009951279cd7e187
Neo2724 <152505943+Neo2724@users.noreply.github.com>
2025-02-26 20:15:42 +0800

    Update Next.js configuration and deploy static site

--- neoportfolio/next.config.js
+++ neoportfolio/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export', // Ensure this line is included for static export
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
